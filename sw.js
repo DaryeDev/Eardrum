c49e7656c861eadca8b1dc3f0e4fca0e6e83722c
Darye <54469750+DaryeDev@users.noreply.github.com>
2021-09-22 14:29:39 +0200

    PWA done, start doing the app
    Included a Online/Offline detector and made a template for the Web app

---
+++ sw.js
@@ -0,0 +1,22 @@
+self.addEventListener("install", e => {
+    console.log("installing...");
+    e.waitUntil(
+        caches.open("static").then(cache => {
+            return cache.addAll(["./", "./index.html", "./src/master.css", "./src/index.js", "./images/logo192.png"]);
+        })
+    );
+});
+
+self.addEventListener("fetch", e => {
+    try {
+        caches.open("static").then(cache => {
+            return cache.addAll(["./", "./index.html", "./src/master.css", "./src/index.js", "./images/logo192.png"]);
+        })
+    } catch (error) {}
+    console.log(e);
+    e.respondWith(
+        caches.match(e.request).then(response => {
+            return response || fetch(e.request);
+        })
+    );
+});
