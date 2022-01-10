c72bcf2a63cb6f09c1614689946ddbd7d077786e
Darye <54469750+DaryeDev@users.noreply.github.com>
2022-01-10 16:47:25 +0100

    si

--- sw.js
+++ sw.js
@@ -1,22 +1,22 @@
 self.addEventListener("install", e => {
-    console.log("installing...");
-    e.waitUntil(
-        caches.open("static").then(cache => {
-            return cache.addAll(["./", "./index.html", "./index.php", "./src/master.css", "./src/index.js", "./images/logo192.png"]);
-        })
-    );
+    // console.log("installing...");
+    // e.waitUntil(
+    //     caches.open("static").then(cache => {
+    //         return cache.addAll(["./", "./index.html", "./index.php", "./src/master.css", "./src/index.js", "./images/logo192.png"]);
+    //     })
+    // );
 });
 
-self.addEventListener("fetch", e => {
-    try {
-        caches.open("static").then(cache => {
-            return cache.addAll(["./", "./index.html", "./index.php", "./src/master.css", "./src/index.js", "./images/logo192.png"]);
-        })
-    } catch (error) {}
-    console.log(e);
-    e.respondWith(
-        caches.match(e.request).then(response => {
-            return response || fetch(e.request);
-        })
-    );
-});
+// self.addEventListener("fetch", e => {
+//     try {
+//         caches.open("static").then(cache => {
+//             return cache.addAll(["./", "./index.html", "./index.php", "./src/master.css", "./src/index.js", "./images/logo192.png"]);
+//         })
+//     } catch (error) {}
+//     console.log(e);
+//     e.respondWith(
+//         caches.match(e.request).then(response => {
+//             return response || fetch(e.request);
+//         })
+//     );
+// });
